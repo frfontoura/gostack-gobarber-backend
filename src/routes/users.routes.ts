0de8489f57b46e41472bd0207dfed86755412acb
frfontoura <frfontoura@gmail.com>
2020-04-17 18:55:32 -0300

    Criação do globalExceptionHandler

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,17 +10,13 @@ const usersRouter = Router();
 const upload = multer(updloadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUserService = new CreateUserService();
-    const user = await createUserService.execute({ name, email, password });
+  const { name, email, password } = request.body;
+  const createUserService = new CreateUserService();
+  const user = await createUserService.execute({ name, email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -28,19 +24,15 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatarService = new UpdateUserAvatarService();
-      const user = await updateUserAvatarService.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updateUserAvatarService = new UpdateUserAvatarService();
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
+import './database';
 
 import express from 'express';
+import 'express-async-errors';
 import routes from './routes';
-import uploadConfig from './config/upload';
 
-import './database';
+import uploadConfig from './config/upload';
+import globalExceptionHandler from './middlewares/globalExceptionHandler';
 
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+app.use(globalExceptionHandler);
 
 app.listen(3333, () => {
   console.log('🚀️ Server started on port 3333');

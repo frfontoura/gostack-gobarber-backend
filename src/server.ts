5450689e166936e4b86c07ea0e67843a99a7adb2
frfontoura <frfontoura@gmail.com>
2020-04-23 18:28:30 -0300

    Habilitando o cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import './database';
 
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 
 import uploadConfig from './config/upload';
@@ -10,6 +11,7 @@ import globalExceptionHandler from './middlewares/globalExceptionHandler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
